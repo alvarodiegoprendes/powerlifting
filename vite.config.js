681e194a5d2d26bd03b1a57e09b690626dd64c51
Alvaro Prendes <alvarodiegoprendes@gmail.com>
2025-03-22 02:24:34 -0400

    vite

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,25 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['vue', 'axios']
+                }
+            }
+        },
+        // Optimizaciones para producción
+        minify: 'terser',
+        chunkSizeWarningLimit: 1000,
+        terserOptions: {
+            compress: {
+                drop_console: true, // Elimina console.logs
+                drop_debugger: true
+            }
+        }
+    }
 });
